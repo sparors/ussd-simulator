d728a783e6aef38463933bcf7e1d9db1687f1307
CyberSai <isaacsai030@gmail.com>
2020-07-14 06:50:57 +0000

    added scrolling for overflow

--- resources/views/livewire/simulator.blade.php
+++ resources/views/livewire/simulator.blade.php
@@ -140,32 +140,36 @@
                             <div class="bg-white h-135 w-72 rounded-2xl px-3 pt-3 pb-8 shadow-xl -mb-12">
                                 <div class="bg-gray-200 h-full rounded-xl">
                                     <div class="flex flex-col h-full space-y-2 p-2">
-                                        <pre class="flex-1"><code class="text-xs text-gray-700 leading-tight tracking-tight overflow-hidden whitespace-pre-wrap break-words">{{ $output }}</code></pre>
-                                        <h4 class="text-blue-700 font-bold">USSD Code</h4>
-                                        <div x-data="{ input: '' }" class="inline-block relative">
-                                            <input x-bind:class="{ 'bg-gray-100 text-blue-800': input !== '', 'bg-gray-400': input === '' }" x-model="input" class="px-5 py-3 form-input rounded-md focus:bg-gray-100 w-full" placeholder="eg.*721#" wire:model="input" type="text" name="input" />
-                                            <div class="absolute inset-y-0 right-0 flex items-center">
-                                                <div wire:model="input">
-                                                    <svg x-on:click="input = '';$dispatch('input', '')" x-show="input !== ''" class="w-6 h-6 text-blue-700 mr-5 cursor-pointer" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path></svg>
+                                        <pre class="flex-1 overflow-y-scroll overflow-x-hidden"><code class="text-xs text-gray-700 leading-tight tracking-tight whitespace-pre-wrap break-words">{{ $output }}</code></pre>
+                                        <div class="flex-none">
+                                            <div class="flex flex-col space-y-2">
+                                                <h4 class="text-blue-700 font-bold">USSD Code</h4>
+                                                <div x-data="{ input: '' }" class="inline-block relative">
+                                                    <input x-bind:class="{ 'bg-gray-100 text-blue-800': input !== '', 'bg-gray-400': input === '' }" x-model="input" class="px-5 py-3 form-input rounded-md focus:bg-gray-100 w-full" placeholder="eg.*721#" wire:model="input" type="text" name="input" />
+                                                    <div class="absolute inset-y-0 right-0 flex items-center">
+                                                        <div wire:model="input">
+                                                            <svg x-on:click="input = '';$dispatch('input', '')" x-show="input !== ''" class="w-6 h-6 text-blue-700 mr-5 cursor-pointer" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path></svg>
+                                                        </div>
+                                                    </div>
                                                 </div>
+                                                <button class="py-3 px-5 bg-blue-700 hover:bg-blue-600 focus:outline-none focus:shadow-outline rounded-md shadow-lg" type="button" wire:click="sendRequest">
+                                                    <span class="flex items-center justify-center space-x-2">
+                                                        <svg class="w-6 h-6 fill-current text-white" viewBox='0 0 512 512'><path d='M476.59,227.05l-.16-.07L49.35,49.84A23.56,23.56,0,0,0,27.14,52,24.65,24.65,0,0,0,16,72.59V185.88a24,24,0,0,0,19.52,23.57l232.93,43.07a4,4,0,0,1,0,7.86L35.53,303.45A24,24,0,0,0,16,327V440.31A23.57,23.57,0,0,0,26.59,460a23.94,23.94,0,0,0,13.22,4,24.55,24.55,0,0,0,9.52-1.93L476.4,285.94l.19-.09a32,32,0,0,0,0-58.8Z'/></svg>
+                                                        <span class="font-bold text-white">
+                                                            Send
+                                                        </span>
+                                                    </span>
+                                                </button>
+                                                <button class="text-blue-700 hover:text-blue-600 py-3 px-5 focus:outline-none focus:shadow-outline rounded-md" type="button" wire:click="cancelRequest">
+                                                    <span class="flex items-center justify-center space-x-2">
+                                                        <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path></svg>
+                                                        <span class="font-bold">
+                                                            Reset
+                                                        </span>
+                                                    </span>
+                                                </button>
                                             </div>
                                         </div>
-                                        <button class="py-3 px-5 bg-blue-700 hover:bg-blue-600 focus:outline-none focus:shadow-outline rounded-md shadow-lg" type="button" wire:click="sendRequest">
-                                            <span class="flex items-center justify-center space-x-2">
-                                                <svg class="w-6 h-6 fill-current text-white" viewBox='0 0 512 512'><path d='M476.59,227.05l-.16-.07L49.35,49.84A23.56,23.56,0,0,0,27.14,52,24.65,24.65,0,0,0,16,72.59V185.88a24,24,0,0,0,19.52,23.57l232.93,43.07a4,4,0,0,1,0,7.86L35.53,303.45A24,24,0,0,0,16,327V440.31A23.57,23.57,0,0,0,26.59,460a23.94,23.94,0,0,0,13.22,4,24.55,24.55,0,0,0,9.52-1.93L476.4,285.94l.19-.09a32,32,0,0,0,0-58.8Z'/></svg>
-                                                <span class="font-bold text-white">
-                                                    Send
-                                                </span>
-                                            </span>
-                                        </button>
-                                        <button class="text-blue-700 hover:text-blue-600 py-3 px-5 focus:outline-none focus:shadow-outline rounded-md" type="button" wire:click="cancelRequest">
-                                            <span class="flex items-center justify-center space-x-2">
-                                                <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path></svg>
-                                                <span class="font-bold">
-                                                    Reset
-                                                </span>
-                                            </span>
-                                        </button>
                                     </div>
                                 </div>
                             </div>
