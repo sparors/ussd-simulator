ec381ce73aa08173d6065af0e283779847fa1fb8
CyberSai <isaacsai030@gmail.com>
2020-07-15 20:13:08 +0000

    Using right colors for inactive state

--- resources/views/livewire/simulator.blade.php
+++ resources/views/livewire/simulator.blade.php
@@ -1,20 +1,20 @@
-<div class="h-screen flex flex-col font-ubuntu">
-    <div class="flex-1 px-32 bg-blue-800">
-        <div class="flex flex-col h-full">
+<div class="h-screen flex flex-col font-ubuntu bg-gray-100">
+    <div class="flex-shrink bg-blue">
+        <div class="flex w-full max-w-screen-lg mx-auto flex-col h-full">
             <h1 class="text-lg flex-initial py-10">
-                <span class="bg-yellow-500 text-blue-800 p-2 rounded-lg font-bold">USSD</span>
+                <span class="bg-yellow text-blue p-2 rounded-lg font-bold">USSD</span>
                 <span class="text-white font-normal">Simulator</span>
             </h1>
 
             <div class="flex-1">
                 <div class="flex h-full">
                     <div class="flex-1">
-                        <h3 class="text-white font-semibold text-xl tracking-wide opacity-75 pb-10">
+                        <h3 class="text-white font-semibold text-xl tracking-wide">
                             Intended for use by developers to test their endpoint <br>
                             implementations before integration.
                         </h3>
 
-                        <div class="space-y-4">
+                        <div class="space-y-4 pt-12 pb-16">
                             <div class="flex items-center">
                                 <label class="sim-label">Host URL</label>
                                 <div x-data="{ url: '' }" class="inline-block relative w-full max-w-sm">
@@ -35,7 +35,7 @@
                                     </div>
                                     <input x-bind:class="{ 'pl-5': method === '', 'pl-16 bg-gray-100 text-blue-800 opacity-100': method !== '' }" x-model="method" x-on:click="hide = false" class="form-input sim-select pr-12 cursor-pointer" wire:model="method" type="text" name="method" placeholder="Select request method" autocomplete="off" readonly />
                                     <div class="absolute inset-y-0 right-0 flex items-center">
-                                        <svg class="w-6 h-6 text-blue-700 mr-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                        <svg class="w-6 h-6 text-ash-dark mr-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                                     </div>
                                     <div x-bind:class="{ 'hidden': hide === true }" x-on:click.away="hide = true" class="absolute bg-gray-100 inset-x-0 rounded-b">
                                         <div wire:model="method">
@@ -64,7 +64,7 @@
                                     </div>
                                     <input x-bind:class="{ 'pl-5': network === '', 'pl-16 bg-gray-100 text-blue-800 opacity-100': network !== '' }" x-model="network" x-on:click="hide = false" class="form-input sim-select pr-12 cursor-pointer" wire:model="network" type="text" name="network" placeholder="Select request network" autocomplete="off" readonly />
                                     <div class="absolute inset-y-0 right-0 flex items-center">
-                                        <svg class="w-6 h-6 text-blue-700 mr-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                        <svg class="w-6 h-6 text-ash-dark mr-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                                     </div>
                                     <div x-bind:class="{ 'hidden': hide === true }" x-on:click.away="hide = true" class="absolute bg-gray-100 inset-x-0 rounded-b">
                                         <div wire:model="network">
@@ -114,7 +114,7 @@
                                     </div>
                                     <input x-bind:class="{ 'pl-5': aggregator === '', 'pl-16 bg-gray-100 text-blue-800 opacity-100': aggregator !== '' }" x-model="aggregator" x-on:click="hide = false" class="form-input sim-select pr-12 cursor-pointer" wire:model="aggregator" type="text" name="aggregator" placeholder="Select request aggregator" autocomplete="off" readonly />
                                     <div class="absolute inset-y-0 right-0 flex items-center">
-                                        <svg class="w-6 h-6 text-blue-700 mr-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                        <svg class="w-6 h-6 text-ash-dark mr-5" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                                     </div>
                                     <div x-bind:class="{ 'hidden': hide === true }" x-on:click.away="hide = true" class="absolute bg-gray-100 inset-x-0 rounded-b">
                                         <div wire:model="aggregator">
@@ -138,21 +138,21 @@
                     <div class="flex-none relative">
                         <div class="absolute right-0 bottom-0">
                             <div class="bg-white h-135 w-72 rounded-2xl px-3 pt-3 pb-8 shadow-xl -mb-12">
-                                <div class="bg-gray-200 h-full rounded-xl">
+                                <div class="bg-ash-light h-full rounded-xl">
                                     <div class="flex flex-col h-full space-y-2 p-2">
-                                        <pre class="flex-1 overflow-y-scroll overflow-x-hidden"><code class="text-xs text-gray-700 leading-tight tracking-tight whitespace-pre-wrap break-words">{{ $output }}</code></pre>
+                                        <pre class="flex-1 overflow-y-auto overflow-x-hidden"><code class="text-xs text-gray-700 leading-tight tracking-tight whitespace-pre-wrap break-words">{{ $output }}</code></pre>
                                         <div class="flex-none">
                                             <div class="flex flex-col space-y-2">
-                                                <h4 class="text-blue-700 font-bold">USSD Code</h4>
+                                                <h4 class="text-blue font-bold">USSD Code</h4>
                                                 <div x-data="{ input: '' }" class="inline-block relative">
-                                                    <input x-bind:class="{ 'bg-gray-100 text-blue-800': input !== '', 'bg-gray-400': input === '' }" x-model="input" class="px-5 py-3 form-input rounded-md focus:bg-gray-100 w-full" placeholder="eg.*721#" wire:model="input" type="text" name="input" />
+                                                    <input x-bind:class="{ 'bg-gray-100 text-blue-800': input !== '', 'bg-ash text-ash-darker': input === '' }" x-model="input" class="px-5 py-3 form-input rounded-md focus:bg-ash w-full" placeholder="eg.*721#" wire:model="input" type="text" name="input" />
                                                     <div class="absolute inset-y-0 right-0 flex items-center">
                                                         <div wire:model="input">
                                                             <svg x-on:click="input = '';$dispatch('input', '')" x-show="input !== ''" class="w-6 h-6 text-blue-700 mr-5 cursor-pointer" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path></svg>
                                                         </div>
                                                     </div>
                                                 </div>
-                                                <button class="py-3 px-5 bg-blue-700 hover:bg-blue-600 focus:outline-none focus:shadow-outline rounded-md shadow-lg" type="button" wire:click="sendRequest">
+                                                <button class="py-3 px-5 bg-blue hover:bg-blue-600 focus:outline-none focus:shadow-outline rounded-md shadow-lg" type="button" wire:click="sendRequest">
                                                     <span class="flex items-center justify-center space-x-2">
                                                         <svg class="w-6 h-6 fill-current text-white" viewBox='0 0 512 512'><path d='M476.59,227.05l-.16-.07L49.35,49.84A23.56,23.56,0,0,0,27.14,52,24.65,24.65,0,0,0,16,72.59V185.88a24,24,0,0,0,19.52,23.57l232.93,43.07a4,4,0,0,1,0,7.86L35.53,303.45A24,24,0,0,0,16,327V440.31A23.57,23.57,0,0,0,26.59,460a23.94,23.94,0,0,0,13.22,4,24.55,24.55,0,0,0,9.52-1.93L476.4,285.94l.19-.09a32,32,0,0,0,0-58.8Z'/></svg>
                                                         <span class="font-bold text-white">
@@ -180,9 +180,9 @@
         </div>
     </div>
 
-    <div class="flex-none flex items-center justify-between py-8 bg-gray-200 text-gray-600 text-xs">
+    <div class="flex-none flex items-center justify-between py-8 text-ash-darkest text-xs bg-white">
         <div class="space-x-5 ml-32">
-            <span class="text-blue-800">Made with
+            <span class="text-blue">Made with
                 <img class="inline-block w-3 h-3" src="{{ config('app.env') === 'production' ? secure_asset('img/heart.svg') : asset('img/heart.svg') }}" />
                 by
             </span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,22 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'blue': {
+          'default': '#05668D',
+          'light': '#197295',
+          '800': '#2C5282'
+        },
+        'yellow': '#FFC45B',
+        'off-white': '#FFFFFFB0',
+        'ash': {
+          'default': '#DDE6E9',
+          'light': '#E5EFF3',
+          'dark': '#B1CFDC',
+          'darker': '#4C545B',
+          'darkest': '#1D3F4D',
+        }
+      },
       fontFamily: {
         'muli': ['Muli', 'sans-serif'],
         'ubuntu': ['Ubuntu', 'sans-serif']
